c03a9d824446315bbab7cbd51ccb1fc5e4a7109b
Odoth <devnull@localhost>
2017-01-19 01:45:58 -0800

    Fix lint errors.

--- ts/src/triggerManager.ts
+++ ts/src/triggerManager.ts
@@ -1,6 +1,6 @@
-import {JsScript} from './jsScript';
-import {Message} from './message';
-import {TrigAlItem} from './trigAlEditBase';
+import {JsScript} from "./jsScript";
+import {Message} from "./message";
+import {TrigAlItem} from "./trigAlEditBase";
 
 declare let $;
 
@@ -15,7 +15,7 @@ export class TriggerManager {
         this.pJsScript = pJsScript;
 
         $(document).ready(() => {
-            var saved_triggers = localStorage.getItem("triggers");
+            let saved_triggers = localStorage.getItem("triggers");
             if (!saved_triggers) {
                 this.triggers = [];
             } else {
@@ -23,11 +23,11 @@ export class TriggerManager {
             }
         });
 
-        this.pMessage.sub('set_triggers_enabled', this.handle_set_triggers_enabled, this);
+        this.pMessage.sub("set_triggers_enabled", this.handle_set_triggers_enabled, this);
     }
 
     public save_triggers() {
-        localStorage.setItem('triggers', JSON.stringify(this.triggers));
+        localStorage.setItem("triggers", JSON.stringify(this.triggers));
     };
 
     private handle_set_triggers_enabled(value) {
@@ -37,35 +37,35 @@ export class TriggerManager {
     public handle_line(line) {
         if (!this.enabled) return;
 //        console.log("TRIGGER: " + line);
-        for (var i=0; i < this.triggers.length; i++) {
-            var trig = this.triggers[i];
+        for (let i = 0; i < this.triggers.length; i++) {
+            let trig = this.triggers[i];
             if (trig.regex) {
-                var match = line.match(trig.pattern);
+                let match = line.match(trig.pattern);
                 if (!match) {
                     continue;
                 }
 
                 if (trig.is_script) {
-                    var script = this.pJsScript.makeScript(trig.value);
-                    if (script) {script()};
+                    let script = this.pJsScript.makeScript(trig.value);
+                    if (script) { script(); };
                 } else {
-                    var value = trig.value;
+                    let value = trig.value;
 
                     value = value.replace(/\$(\d+)/g, function(m, d) {
-                        return match[parseInt(d)] || '';
+                        return match[parseInt(d)] || "";
                     });
 
-                    var cmds = value.replace('\r', '').split('\n');
-                    this.pMessage.pub('trigger_send_commands', {cmds: cmds});
+                    let cmds = value.replace("\r", "").split("\n");
+                    this.pMessage.pub("trigger_send_commands", {cmds: cmds});
                 }
             } else {
                 if (line.includes(trig.pattern)) {
                     if (trig.is_script) {
-                        var script = this.pJsScript.makeScript(trig.value);
-                        if (script) {script()};
+                        let script = this.pJsScript.makeScript(trig.value);
+                        if (script) { script(); };
                     } else {
-                        var cmds = trig.value.replace('\r', '').split('\n');
-                        this.pMessage.pub('trigger_send_commands', {cmds: cmds});
+                        let cmds = trig.value.replace("\r", "").split("\n");
+                        this.pMessage.pub("trigger_send_commands", {cmds: cmds});
                     }
                 }
             }
