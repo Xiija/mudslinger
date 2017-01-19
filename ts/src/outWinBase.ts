421f83438ef330c9b4d1737e90b74f5f3c17b21d
Odoth <devnull@localhost>
2017-01-19 01:31:21 -0800

    Fix lint errors.

--- ts/src/outWinBase.ts
+++ ts/src/outWinBase.ts
@@ -1,4 +1,4 @@
-import * as Util from './util';
+import * as Util from "./util";
 
 declare let $;
 
@@ -27,10 +27,10 @@ export class OutWinBase {
 
     private scroll_lock = false; // true when we should not scroll to bottom
     private handle_scroll(e) {
-        var scrollHeight = this.root_elem.prop('scrollHeight');
-        var scrollTop = this.root_elem.scrollTop();
-        var outerHeight = this.root_elem.outerHeight();
-        var is_at_bottom = outerHeight + scrollTop >= scrollHeight;
+        let scrollHeight = this.root_elem.prop("scrollHeight");
+        let scrollTop = this.root_elem.scrollTop();
+        let outerHeight = this.root_elem.outerHeight();
+        let is_at_bottom = outerHeight + scrollTop >= scrollHeight;
 
         this.scroll_lock = !is_at_bottom;
     }
@@ -42,10 +42,10 @@ export class OutWinBase {
         this.target_elems = [elem];
         this.target =  elem;
 
-        // direct children of the root will be line containers, let's push the first one.
-        this.push_elem($('<span>').appendTo(elem));
+        // direct children of the root will be line containers, let"s push the first one.
+        this.push_elem($("<span>").appendTo(elem));
 
-        this.root_elem.bind('scroll', (e) => {this.handle_scroll(e);});
+        this.root_elem.bind("scroll", (e) => { this.handle_scroll(e); });
     };
 
     // elem is the actual jquery element
@@ -63,11 +63,11 @@ export class OutWinBase {
     public pop_elem() {
         this.write_buffer();
 
-        var popped = this.target_elems.pop();
+        let popped = this.target_elems.pop();
 //        console.log(o);
 //        console.log("elem popped");
 //        console.log(popped);
-        this.target = this.target_elems[this.target_elems.length-1];
+        this.target = this.target_elems[this.target_elems.length - 1];
         return popped;
     }
 
@@ -75,56 +75,56 @@ export class OutWinBase {
         // default to nothing, main output window will send it to trigger manager
     }
 
-    private append_buffer = '';
-    private line_text = ''; // track full text of the line with no escape sequences or tags
+    private append_buffer = "";
+    private line_text = ""; // track full text of the line with no escape sequences or tags
     public add_text(txt) {
         this.line_text += txt;
-        var html = Util.raw_to_html(txt);
-//        var span = $(document.createElement('span'));
-        var span_text = '<span'
-        var style = ''
+        let html = Util.raw_to_html(txt);
+//        let span = $(document.createElement("span"));
+        let span_text = "<span";
+        let style = "";
         if (this.fg_color || this.bg_color) {
-            style = ' style="'
+            style = " style=\"";
             if (this.fg_color) {
-                style += 'color:' + this.fg_color + ';';
+                style += "color:" + this.fg_color + ";";
             }
             if (this.bg_color) {
-                style += 'background-color:' + this.bg_color + ';';
+                style += "background-color:" + this.bg_color + ";";
             }
-            style += '"';
+            style += "\"";
         }
-        span_text += style + '>';
+        span_text += style + ">";
         span_text += html;
-        span_text += '</span>';
+        span_text += "</span>";
         this.append_buffer += span_text;
 
-        if (txt.endsWith('\n')) {
+        if (txt.endsWith("\n")) {
             this.target.append(this.append_buffer);
-            this.append_buffer = '';
+            this.append_buffer = "";
             this.new_line();
         }
     };
 
     private new_line() {
         this.pop_elem(); // pop the old line
-        this.push_elem($('<span>').appendTo(this.target));
+        this.push_elem($("<span>").appendTo(this.target));
 
         this.handle_line(this.line_text);
-        this.line_text = '';
+        this.line_text = "";
 
         this.line_count += 1;
         if (this.line_count > this.max_lines) {
             this.root_elem.children(":lt(" +
-                (this.max_lines/2) +
+                (this.max_lines / 2) +
                 ")"
             ).remove();
-            this.line_count = (this.max_lines/2);
+            this.line_count = (this.max_lines / 2);
         }
     }
 
     private write_buffer() {
         this.target.append(this.append_buffer);
-        this.append_buffer = '';
+        this.append_buffer = "";
     };
 
     public output_done() {
@@ -135,28 +135,28 @@ export class OutWinBase {
     private scroll_requested = false;
     private _scroll_bottom() {
         console.time("_scroll_bottom");
-        var elem = this.root_elem;
-        elem.scrollTop(elem.prop('scrollHeight'));
+        let elem = this.root_elem;
+        elem.scrollTop(elem.prop("scrollHeight"));
         this.scroll_lock = false;
         this.scroll_requested = false;
         console.timeEnd("_scroll_bottom");
     };
 
     protected scroll_bottom(force: boolean = false) {
-        if (this.scroll_lock && !(force == true)) {
+        if (this.scroll_lock && force !== true) {
             return;
         }
         if (this.scroll_requested) {
             return;
         }
-        var o = this;
+        let o = this;
         requestAnimationFrame(() => o._scroll_bottom.call(o));
         this.scroll_requested = true;
 //        //if (true) {return;}
-//        var elem = o.root_elem;
-//        //var scrollHeight = elem.prop('scrollHeight');
+//        let elem = o.root_elem;
+//        //let scrollHeight = elem.prop("scrollHeight");
 //        console.log("scroll calt");
-////        var scrollHeight = elem[0].scrollHeight;
+////        let scrollHeight = elem[0].scrollHeight;
 //        elem.scrollTop(line_count * 20);
     }
 }
