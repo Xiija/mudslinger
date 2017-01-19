d6d0ccedf64feefa37ec2db59402d5bfdce99ce6
Odoth <devnull@localhost>
2017-01-19 00:39:29 -0800

    Fix lint errors.

--- ts/src/chatWin.ts
+++ ts/src/chatWin.ts
@@ -1,29 +1,29 @@
-import {Message} from './message';
-import {OutWinBase} from './outWinBase';
+import {Message} from "./message";
+import {OutWinBase} from "./outWinBase";
 
 declare let $;
 
 export class ChatWin extends OutWinBase {
     private pMessage: Message;
-    
+
     private html;
 
     constructor(pMessage: Message) {
         super();
         this.pMessage = pMessage;
 
-        this.pMessage.sub('prepare_reload_layout', this.prepare_reload_layout, this);
-        this.pMessage.sub('load_layout', this.load_layout, this);
+        this.pMessage.sub("prepare_reload_layout", this.prepare_reload_layout, this);
+        this.pMessage.sub("load_layout", this.load_layout, this);
     }
 
     private prepare_reload_layout() {
-        this.html = $('#win_chat').html();
+        this.html = $("#win_chat").html();
     };
 
     private load_layout() {
-        this.set_root_elem($('#win_chat'));
+        this.set_root_elem($("#win_chat"));
         if (this.html) {
-            $('#win_chat').html(this.html);
+            $("#win_chat").html(this.html);
             this.html = null;
         }
     }
